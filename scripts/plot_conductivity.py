f0e04ca22eca19a626fa6e6334750350dce9491c
williamdparker <parkerw@uwp.edu>
2024-06-26 12:16:45 -0500

    Tried using dictionaries to organize conductivities

--- scripts/plot_conductivity.py
+++ scripts/plot_conductivity.py
@@ -9,26 +9,53 @@ thermaldatatable = pd.read_html("https://www.angstromsciences.com/thermal-conduc
 
 electricaldatatable['Electrical Conductivity'] = electricaldatatable['Electrical Conductivity'].str.split().str[0]
 thermaldatatable['Thermal Conductivity'] = thermaldatatable['Thermal Conductivity'].str.split().str[0]
+
 completedatatable = []
 remove = [1, 2]
+
 electricaldatatable = electricaldatatable.to_numpy()
 electricaldatatable = np.delete(electricaldatatable, remove, axis=1)
+
 thermaldatatable = thermaldatatable.to_numpy()
 thermaldatatable = np.delete(thermaldatatable, remove, axis=1)
+
 electricaldatatable = np.fliplr(electricaldatatable)
 thermaldatatable = np.fliplr(thermaldatatable)
+
 sorted = np.argsort(electricaldatatable[:, 0])
 electricaldatatable = electricaldatatable[sorted]
+
 sorted = np.argsort(thermaldatatable[:, 0])
 thermaldatatable = thermaldatatable[sorted]
-#print(electricaldatatable)
-#print(thermaldatatable)
+
+# print(electricaldatatable)
+# print(thermaldatatable)
 #for x in thermaldatatable[:, 0]:
 #    addrow = [x, thermaldatatable[x, 1], electricaldatatable[x, 1]]
 #    completedatatable = numpy.vstack([completedatatable, ])
 #    print(electricaldatatable[x, 0])
 #    print(addrow)
-for x in thermaldatatable[:, 0]:
-    if thermaldatatable[x, 0] == electricaldatatable[x, 0]:
-        addrow = [x, thermaldatatable[x, 1], electricaldatatable[x, 1]]
-        print(addrow)
+# for x in thermaldatatable[:, 0]:
+#     if thermaldatatable[x, 0] == electricaldatatable[x, 0]:
+#         addrow = [x, thermaldatatable[x, 1], electricaldatatable[x, 1]]
+#         print(addrow)
+
+conductivities = {}
+
+for atomic_number in range(1, 105):
+    # electrical = False
+    # thermal = False
+    for atomic_number_inside, electrical_conductivity in electricaldatatable:
+        if int(atomic_number_inside) == atomic_number:
+            conductivities[atomic_number] = {'electrical conductivity': electrical_conductivity}
+    for atomic_number_inside, thermal_conductivity in thermaldatatable:
+        if int(atomic_number_inside) == atomic_number:
+            if int(atomic_number_inside) in conductivities.keys():
+                conductivities[atomic_number]['thermal conductivity'] == thermal_conductivity
+            else:
+                conductivities[atomic_number] = {'thermal conductivity': thermal_conductivity}
+
+
+print(conductivities)
+
+
