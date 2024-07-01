a97c36ae8c71d0f47112c599d48b204c369c8e61
FrankFelerski <knarfruler@gmail.com>
2024-07-01 13:09:36 -0500

    Merged and sorted tables, need to clean up quotes around numbers and split numbers involving E into real float values

--- scripts/plot_conductivity.py
+++ scripts/plot_conductivity.py
@@ -1,4 +1,3 @@
-import numpy
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,27 +8,20 @@ thermaldatatable = pd.read_html("https://www.angstromsciences.com/thermal-conduc
 
 electricaldatatable['Electrical Conductivity'] = electricaldatatable['Electrical Conductivity'].str.split().str[0]
 thermaldatatable['Thermal Conductivity'] = thermaldatatable['Thermal Conductivity'].str.split().str[0]
-
 completedatatable = []
 remove = [1, 2]
-
 electricaldatatable = electricaldatatable.to_numpy()
 electricaldatatable = np.delete(electricaldatatable, remove, axis=1)
-
 thermaldatatable = thermaldatatable.to_numpy()
 thermaldatatable = np.delete(thermaldatatable, remove, axis=1)
-
 electricaldatatable = np.fliplr(electricaldatatable)
 thermaldatatable = np.fliplr(thermaldatatable)
-
 sorted = np.argsort(electricaldatatable[:, 0])
 electricaldatatable = electricaldatatable[sorted]
-
 sorted = np.argsort(thermaldatatable[:, 0])
 thermaldatatable = thermaldatatable[sorted]
-
-# print(electricaldatatable)
-# print(thermaldatatable)
+#print(electricaldatatable)
+#print(thermaldatatable)
 #for x in thermaldatatable[:, 0]:
 #    addrow = [x, thermaldatatable[x, 1], electricaldatatable[x, 1]]
 #    completedatatable = numpy.vstack([completedatatable, ])
@@ -38,24 +30,26 @@ thermaldatatable = thermaldatatable[sorted]
 # for x in thermaldatatable[:, 0]:
 #     if thermaldatatable[x, 0] == electricaldatatable[x, 0]:
 #         addrow = [x, thermaldatatable[x, 1], electricaldatatable[x, 1]]
-#         print(addrow)
+#        print(addrow)
+#conductivities = {}
 
-conductivities = {}
-
-for atomic_number in range(1, 105):
-    # electrical = False
-    # thermal = False
-    for atomic_number_inside, electrical_conductivity in electricaldatatable:
-        if int(atomic_number_inside) == atomic_number:
-            conductivities[atomic_number] = {'electrical conductivity': electrical_conductivity}
-    for atomic_number_inside, thermal_conductivity in thermaldatatable:
-        if int(atomic_number_inside) == atomic_number:
-            if int(atomic_number_inside) in conductivities.keys():
-                conductivities[atomic_number]['thermal conductivity'] == thermal_conductivity
-            else:
-                conductivities[atomic_number] = {'thermal conductivity': thermal_conductivity}
-
-
-print(conductivities)
-
-
+#for atomic_number in range(1, 105):
+#    for atomic_number_inside, electrical_conductivity in electricaldatatable:
+#        if int(atomic_number_inside) == atomic_number:
+#            conductivities[atomic_number] = {'electrical conductivity': electrical_conductivity}
+#    for atomic_number_inside, thermal_conductivity in thermaldatatable:
+#        if int(atomic_number_inside) == atomic_number:
+#            if int(atomic_number_inside) in conductivities.keys():
+#                conductivities[atomic_number]['thermal conductivity'] == thermal_conductivity
+#            else:
+#                conductivities[atomic_number] = {'thermal conductivity': thermal_conductivity}
+#print(conductivities)
+for X in range(0, 104):
+    for Y in range(0, 78):
+        if thermaldatatable[X, 0] == electricaldatatable[Y, 0]:
+            conductivitytemp = [thermaldatatable[X, 0], thermaldatatable[X, 1], electricaldatatable[Y, 1]]
+#            print(conductivitytemp)
+            completedatatable.append(conductivitytemp)
+        else:
+            pass
+print(completedatatable)
