b336b804966b393b12ec1867b3ae4fa55843329d
williamdparker <parkerw@uwp.edu>
2024-07-18 12:51:24 -0500

    Worked on formatting plot

--- scripts/plot_conductivity.py
+++ scripts/plot_conductivity.py
@@ -2,8 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import matplotlib
+
+
+matplotlib.use('macosx')
+
 electricaldatatable = pd.read_html("https://www.angstromsciences.com/elements-electrical-conductivity")[0]
 thermaldatatable = pd.read_html("https://www.angstromsciences.com/thermal-conductivity-of-elements")[0]
+
+print(thermaldatatable)
 
 
 electricaldatatable['Electrical Conductivity'] = electricaldatatable['Electrical Conductivity'].str.split().str[0]
@@ -47,11 +54,30 @@ thermaldatatable = thermaldatatable[sorted]
 for X in range(0, 104):
     for Y in range(0, 78):
         if thermaldatatable[X, 0] == electricaldatatable[Y, 0]:
-            conductivitytemp = [int(thermaldatatable[X, 0]), float(thermaldatatable[X, 1]), float(electricaldatatable[Y, 1])]
+            conductivitytemp = [int(thermaldatatable[X, 0]), float(thermaldatatable[X, 1])*1e2, float(electricaldatatable[Y, 1])/1e8]
             completedatatable.append(conductivitytemp)
         else:
             pass
 completedatatable = np.array(completedatatable)
+# print(completedatatable)
+
+atomic_numbers = completedatatable.transpose()[0]
+thermal_conductivities = completedatatable.transpose()[1]
+electrical_conductivities = completedatatable.transpose()[2]
+
+plt.plot(thermal_conductivities, electrical_conductivities, marker='o', linestyle='', markersize=4)
+
+for atomic_number, thermal, electrical in zip(atomic_numbers, thermal_conductivities, electrical_conductivities):
+    if electrical < 1e-11:
+        plt.text(thermal, electrical, int(atomic_number), ha='center', va='bottom')
+
 #print(completedatatable.transpose()[1])
-plt.plot(completedatatable.transpose()[1], completedatatable.transpose()[2], marker='o', linestyle='')
+#plt.plot(completedatatable.transpose()[1], completedatatable.transpose()[2], marker='o', linestyle='')
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.xlabel(r'$\kappa$ [W/(m K)]')
+plt.ylabel(r'$\sigma$ [S/m]')
+
 plt.show()
