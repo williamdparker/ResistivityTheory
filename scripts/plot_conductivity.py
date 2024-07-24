d09c0beee80649d591ba4d90e56ee13f1819ff93
FrankFelerski <knarfruler@gmail.com>
2024-07-23 20:00:41 -0500

    Updated to plot the data, now with colors!

--- scripts/plot_conductivity.py
+++ scripts/plot_conductivity.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-matplotlib.use('macosx')
+#matplotlib.use('macosx')
 
 electricaldatatable = pd.read_html("https://www.angstromsciences.com/elements-electrical-conductivity")[0]
 thermaldatatable = pd.read_html("https://www.angstromsciences.com/thermal-conductivity-of-elements")[0]
 
-print(thermaldatatable)
+#print(thermaldatatable)
 
 
 electricaldatatable['Electrical Conductivity'] = electricaldatatable['Electrical Conductivity'].str.split().str[0]
@@ -59,17 +59,31 @@ for X in range(0, 104):
         else:
             pass
 completedatatable = np.array(completedatatable)
-# print(completedatatable)
+#print(completedatatable)
 
 atomic_numbers = completedatatable.transpose()[0]
 thermal_conductivities = completedatatable.transpose()[1]
 electrical_conductivities = completedatatable.transpose()[2]
+S_Block = [3, 4, 11, 12, 19, 20, 37, 38, 55, 56, 87, 88] #Red
+P_Block = [5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 31, 32, 33, 34, 35, 36, 49, 50, 51, 52, 53, 54, 81, 82, 83, 84, 85, 86] #Blue
+D_Block = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80] #Green
+F_Block = [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 89, 90, 91, 92, 93, 94] #Orange
 
-plt.plot(thermal_conductivities, electrical_conductivities, marker='o', linestyle='', markersize=4)
+#plt.plot(thermal_conductivities, electrical_conductivities, marker='o', linestyle='', markersize=4)
 
 for atomic_number, thermal, electrical in zip(atomic_numbers, thermal_conductivities, electrical_conductivities):
     if electrical < 1e-11:
         plt.text(thermal, electrical, int(atomic_number), ha='center', va='bottom')
+
+for atomic_number, thermal, electrical, in zip(atomic_numbers, thermal_conductivities, electrical_conductivities):
+    if atomic_number in S_Block:
+        plt.plot(thermal, electrical, color='red', marker='o', linestyle='', markersize=4)
+    elif atomic_number in P_Block:
+        plt.plot(thermal, electrical, color='blue', marker='o', linestyle='', markersize=4)
+    elif atomic_number in D_Block:
+        plt.plot(thermal, electrical, color='green', marker='o', linestyle='', markersize=4)
+    elif atomic_number in F_Block:
+        plt.plot(thermal, electrical, color='orange', marker='o', linestyle='', markersize=4)
 
 #print(completedatatable.transpose()[1])
 #plt.plot(completedatatable.transpose()[1], completedatatable.transpose()[2], marker='o', linestyle='')
